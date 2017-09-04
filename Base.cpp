8093b5693cfb6b8c567ee83e855a582addd7174c
Slava Bezgachev <bezgachev@gmail.com>
2017-09-04 11:27:02 +0100

    Constructor done (but not quite)

--- Base.cpp
+++ Base.cpp
@@ -21,36 +21,82 @@ Base::Base(ZergBot bot, bool findNew)
 
 		Point2D spawn = bot.GetBuildingManager().GetSpawn();
 		HatcheryPosition = spawn;
-
-		// Find nearby mineral patches
-		Units patches;
-		std::vector<UNIT_TYPEID> mineralTypes;
-		mineralTypes.push_back(UNIT_TYPEID::NEUTRAL_MINERALFIELD);
-		mineralTypes.push_back(UNIT_TYPEID::NEUTRAL_MINERALFIELD750);
-		Units allPatches = Util::GetNeutralUnitsOfType(bot, mineralTypes);
-
-		for (Unit patch : allPatches)
-		{
-			if (Distance2D(patch.pos, HatcheryPosition) < 15.0)
-			{
-				patches.push_back(patch);
-			}
-		}
-
-		mineralPatches = patches;
 	}
+
+	// Find nearby mineral patches, set to the instance
+	Units patches;
+	std::vector<UNIT_TYPEID> mineralTypes;
+	mineralTypes.push_back(UNIT_TYPEID::NEUTRAL_MINERALFIELD);
+	mineralTypes.push_back(UNIT_TYPEID::NEUTRAL_MINERALFIELD750);
+	mineralTypes.push_back(UNIT_TYPEID::NEUTRAL_LABMINERALFIELD);
+	mineralTypes.push_back(UNIT_TYPEID::NEUTRAL_LABMINERALFIELD750);
+	mineralTypes.push_back(UNIT_TYPEID::NEUTRAL_RICHMINERALFIELD);
+	mineralTypes.push_back(UNIT_TYPEID::NEUTRAL_RICHMINERALFIELD750);
+
+	Units allPatches = Util::GetNeutralUnitsOfType(bot, mineralTypes);
+
+	for (Unit patch : allPatches)
+	{
+		if (Distance2D(patch.pos, HatcheryPosition) < 15.0)
+		{
+			patches.push_back(patch);
+		}
+	}
+
+	mineralPatches = patches;
+
+	if (mineralPatches[0].unit_type == UNIT_TYPEID::NEUTRAL_RICHMINERALFIELD || mineralPatches[0].unit_type == UNIT_TYPEID::NEUTRAL_RICHMINERALFIELD750)
+	{
+		isRichMinerals = true;
+	} else
+	{
+		isRichMinerals = false;
+	}
+
+	// Repeat for geysers
+	Units geysers;
+	std::vector<UNIT_TYPEID> geyserTypes;
+	geyserTypes.push_back(UNIT_TYPEID::NEUTRAL_VESPENEGEYSER);
+	geyserTypes.push_back(UNIT_TYPEID::NEUTRAL_PROTOSSVESPENEGEYSER);
+	geyserTypes.push_back(UNIT_TYPEID::NEUTRAL_SPACEPLATFORMGEYSER);
+
+	Units allGeysers = Util::GetNeutralUnitsOfType(bot, geyserTypes);
+
+	for (Unit geyser : allGeysers)
+	{
+		if (Distance2D(geyser.pos, HatcheryPosition) < 15.0)
+		{
+			geysers.push_back(geyser);
+		}
+	}
+
+	if (geysers.size() < 1)
+	{
+		hasGeysers = false;
+	} else if (geysers.size() == 1)
+	{
+		geyserOne = geysers[0];
+	} else if (geysers.size() > 1)
+	{
+		geyserOne = geysers[0];
+		geyserTwo = geysers[1];
+	}
+
 }
 
 Point2D Base::GetHatcheryPosition()
 {
+	return HatcheryPosition;
 }
 
 bool Base::GetIsRichMineral()
 {
+	return isRichMinerals;
 }
 
 bool Base::GetHasGeysers()
 {
+	return hasGeysers;
 }
 
 void Base::OnStep()
