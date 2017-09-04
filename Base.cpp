26d56301927937e446335a51da1bf0e7f30336b6
Slava Bezgachev <bezgachev@gmail.com>
2017-09-04 11:08:43 +0100

    Added the iniital structure of the Base class that will help with base management and resources

--- Base.cpp
+++ Base.cpp
@@ -1,12 +1,59 @@
 #include "Base.h"
+#include <sc2api/sc2_common.h>
+#include <sc2api/sc2_agent.h>
+#include "ZergBot.h"
+#include "Utilities.h"
+
+using namespace sc2;
 
 
 
-Base::Base()
+Base::Base(ZergBot bot, bool findNew)
+{
+	if (findNew)
+	{
+		// Find a new base, set all the properties
+	}
+	else
+	{
+		// Use the first hatchery as the starting point
+		const ObservationInterface* obs = bot.Observation();
+
+		Point2D spawn = bot.GetBuildingManager().GetSpawn();
+		HatcheryPosition = spawn;
+
+		// Find nearby mineral patches
+		Units patches;
+		std::vector<UNIT_TYPEID> mineralTypes;
+		mineralTypes.push_back(UNIT_TYPEID::NEUTRAL_MINERALFIELD);
+		mineralTypes.push_back(UNIT_TYPEID::NEUTRAL_MINERALFIELD750);
+		Units allPatches = Util::GetNeutralUnitsOfType(bot, mineralTypes);
+
+		for (Unit patch : allPatches)
+		{
+			if (Distance2D(patch.pos, HatcheryPosition) < 15.0)
+			{
+				patches.push_back(patch);
+			}
+		}
+
+		mineralPatches = patches;
+	}
+}
+
+Point2D Base::GetHatcheryPosition()
 {
 }
 
-
-Base::~Base()
+bool Base::GetIsRichMineral()
 {
 }
+
+bool Base::GetHasGeysers()
+{
+}
+
+void Base::OnStep()
+{
+}
+
--- Base.h
+++ Base.h
@@ -1,8 +1,41 @@
 #pragma once
+#include <sc2api/sc2_common.h>
+#include <sc2api/sc2_unit.h>
+#include "ZergBot.h";
+
+using namespace sc2;
+
 class Base
 {
+	ZergBot bot;
+
+	Point2D HatcheryPosition;
+	Units mineralPatches;
+	Units geysers;
+	bool isRichMinerals;
+	bool hasGeysers;
+
+	Unit geyserOne;
+	Unit geyserTwo;
+
+	size_t dronesOnMinerals;
+	size_t dronesOnGeyserOne;
+	size_t dronesOnGeyserTwo;
 
 public:
-	Base();
+
+	/**
+	 * Constructor for the initial base
+	 */
+	Base(ZergBot bot, bool findNew);
+
+	Point2D GetHatcheryPosition();
+
+	bool GetIsRichMineral();
+	bool GetHasGeysers();
+
+	void OnStep();
+
+	
 };
 
